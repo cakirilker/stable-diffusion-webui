b0f73085d7c9e56ecb6aee825a67d7572e578f5c
ilker-cakir <ilker.cakir@auto1.com>
2023-10-14 18:26:56 +0200

    update

--- webui.sh
+++ webui.sh
@@ -111,14 +111,14 @@ then
     exit 1
 fi
 
-if [[ -d .git ]]
-then
-    printf "\n%s\n" "${delimiter}"
-    printf "Repo already cloned, using it as install directory"
-    printf "\n%s\n" "${delimiter}"
-    install_dir="${PWD}/../"
-    clone_dir="${PWD##*/}"
-fi
+# if [[ -d .git ]]
+# then
+#     printf "\n%s\n" "${delimiter}"
+#     printf "Repo already cloned, using it as install directory"
+#     printf "\n%s\n" "${delimiter}"
+install_dir="${PWD}/../"
+clone_dir="${PWD##*/}"
+# fi
 
 # Check prerequisites
 gpu_info=$(lspci 2>/dev/null | grep -E "VGA|Display")
